5ab60ef23f3645ff7deed209aced7d1a99dc13ee
Matheus Peixoto <matheus-pxt@hotmail.com>
2022-07-13 21:04:28 -0300

    handling exceptions and adding validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-
-import { Product } from './product/entities/product.entity';
-import { Supplier } from './supplier/entities/supplier.entity';
 
 @Module({
   imports: [
@@ -18,13 +14,12 @@ import { Supplier } from './supplier/entities/supplier.entity';
       username: 'root',
       password: 'root',
       database: 'product_ms',
-      entities: [Product, Supplier],
+      autoLoadEntities: true,
       synchronize: true,
+      logging: ['query', 'error'],
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  constructor(private dataSource: DataSource) {}
-}
+export class AppModule {}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { ProductDto } from './dto/product.dto';
 import { Product } from './entities/product.entity';
 
@@ -26,21 +26,58 @@ export class ProductService {
 
     const supplierDatabase =
       await this.supplierService.findSupplierBySupplierId(supplier.supplierId);
+
     if (supplierDatabase) {
       newProduct.supplier = supplierDatabase;
     } else {
       newProduct.supplier = supplier;
     }
 
+    const product = await this.productRepository.findOneBy({ productId });
+    if (product) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: `ProductId ${product.productId} already exists`,
+          error: 'Bad Request',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     return await this.productRepository.save(newProduct);
   }
 
   findAll(): Promise<Product[]> {
-    return this.productRepository.find();
+    return this.productRepository.find({
+      relations: {
+        supplier: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return this.productRepository.findOneBy({ id: id });
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({
+      where: {
+        id: id,
+      },
+      relations: {
+        supplier: true,
+      },
+    });
+
+    if (product) {
+      return product;
+    }
+
+    throw new HttpException(
+      {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: `Product ${id} could not be found`,
+        error: 'Not found',
+      },
+      HttpStatus.NOT_FOUND,
+    );
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
